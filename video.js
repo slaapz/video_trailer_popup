3e29a4b76eeb78a84562f33626b03a5ffc6fcb3f
Patrick Ballm <slaapz@gmail.com>
2023-03-26 14:27:16 -0600

    Added JS to open and close the trailer video

---
+++ video.js
@@ -0,0 +1,11 @@
+const trailerEl = document.querySelector('.trailer-container');
+const closeEl = document.querySelector('.close-icon');
+const button = document.querySelector('.btn');
+
+button.addEventListener('click', () =>{
+    trailerEl.classList.add('active');
+});
+
+closeEl.addEventListener('click',() => {
+    trailerEl.classList.remove('active');
+});
